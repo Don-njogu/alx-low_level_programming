e9cfef36f5b7764ff950f67948fb80ba66ebb4aa
Don-njogu <njogunganga9119@gmail.com>
2023-02-17 08:00:51 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+int main() {
+    int i, j, k, l;
+
+    for (i = 0; i <= 9; i++) {
+        for (j = i + 1; j <= 9; j++) {
+            for (k = j + 1; k <= 9; k++) {
+                for (l = k + 1; l <= 9; l++) {
+                    putchar('0' + i);
+                    putchar(',');
+                    putchar(' ');
+                    putchar('0' + j);
+                    putchar(',');
+                    putchar(' ');
+                    putchar('0' + k);
+                    putchar(',');
+                    putchar(' ');
+                    putchar('0' + l);
+                    putchar('\n');
+                }
+            }
+        }
+    }
+
+    return 0;
+}
+
